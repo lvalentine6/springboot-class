cb7b3706e80ca483ca452a48ca47e0941d1dd953
lvalentine6 <ysr1227@naver.com>
2022-06-04 03:02:40 +0900

    API

--- src/main/java/com/lecture/spring/springbootclass/controller/IndexController.java
+++ src/main/java/com/lecture/spring/springbootclass/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.lecture.spring.springbootclass.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -11,5 +13,47 @@ public class IndexController {
     public String hello(Model model) {
         model.addAttribute("data", "boot Project");
         return "hello";
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("/hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("/hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name, @RequestParam("age") int age) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        hello.setAge(age);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+        private int age;
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
